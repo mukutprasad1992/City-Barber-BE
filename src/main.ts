24924a3489242ca39003b89e2bd7e1166485347b
Mukut Prasad <mukutprasad1992@gmail.com>
2023-09-14 13:38:27 +0530

    resolved

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ declare const module: any;
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*"
+  });
+
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();

c06e1b64d8a34268b0a5096d87c0d14e365a645b
Mukut Prasad <mukutprasad1992@gmail.com>
2023-09-14 13:17:26 +0530

    Resolve cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ declare const module: any;
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: false
-  })
+  app.enableCors();
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
